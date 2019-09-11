47746a22bdc3bab66ce15d961405c4c3dca6ca5b
Audai Al-Fandi <addaui@outlook.com>
2019-09-11 11:12:50 +0300

    Initial Commit

---
+++ harajTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  harajTest
+//
+//  Created by Audai Al-Fandi on 11/09/2019.
+//  Copyright © 2019 Audai Al-Fandi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
