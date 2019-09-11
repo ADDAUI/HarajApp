73fb029005bb38bea3a24b54327c10d711475bca
Audai Al-Fandi <addaui@outlook.com>
2019-09-11 14:56:06 +0300

    trackFiles

---
+++ harajTest/Controllers/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  harajTest
+//
+//  Created by Audai Al-Fandi on 11/09/2019.
+//  Copyright © 2019 Audai Al-Fandi. All rights reserved.
+//
+
+
+import UIKit
+import Alamofire
+
+class DetailViewController: UIViewController {
+    
+    //Consts and Vars
+    var selectedPosting:Posting? = nil
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = selectedPosting?.name
+        Alamofire.request(selectedPosting!.url, method: .get).validate()
+            .responseData(completionHandler: { (responseData) in
+                self.image.image = UIImage(data: responseData.data!)
+            })
+
+    }    
+}
